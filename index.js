af68630a82c1ebfe74e8a1a4e50f6c700171577b
Brentonius <bjavonjackson@gmail.com>
2020-02-28 10:50:04 -0500

    added scroll to top button, not working tho

---
+++ index.js
@@ -0,0 +1,27 @@
+// Scroll to top button functionality
+
+//Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// Show button when user scrolls down 50px, otherwise hide
+function scrollFunction () {
+	if (document.body.scrollTop > 50) {
+		mybutton.style.display = "block";
+		mybutton.style.position = "fixed";
+	}
+	else {
+		mybutton.style.display = "none";
+	}
+}
+
+// Set function to activate when user scrolls
+document.body.addEventListener("scroll", scrollFunction);
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// Add click functionality to button
+mybutton.addEventListener("click", topFunction);
